a2a73d77923e880c9a9c25fc031f2ba5ba60a5e4
VojtaB <triplkrypl@centrum.cz>
2022-04-09 20:01:54 +0200

    http action parameters validation support

--- src/Factory/Server/Validator.php
+++ src/Factory/Server/Validator.php
@@ -6,11 +6,25 @@ namespace SimpleAsFuck\ApiToolkit\Factory\Server;
 
 use Psr\Http\Message\ServerRequestInterface;
 use SimpleAsFuck\ApiToolkit\Model\Server\RequestRules;
+use SimpleAsFuck\Validator\Model\RuleChain;
+use SimpleAsFuck\Validator\Model\Validated;
+use SimpleAsFuck\Validator\Rule\String\StringRule;
 
 final class Validator
 {
     public static function make(ServerRequestInterface $request): RequestRules
     {
         return new RequestRules(new ApiValidationException(), $request);
+    }
+
+    /**
+     * @param non-empty-string $name
+     * @return StringRule
+     */
+    public static function parameter(?string $value, string $name): StringRule
+    {
+        /** @var Validated<mixed> $value */
+        $value = new Validated($value);
+        return new StringRule(new ApiValidationException(), new RuleChain(), $value, 'Url path parameter: '.$name);
     }
 }
--- src/Factory/Symfony/Validator.php
+++ src/Factory/Symfony/Validator.php
@@ -6,6 +6,9 @@ namespace SimpleAsFuck\ApiToolkit\Factory\Symfony;
 
 use GuzzleHttp\Psr7\HttpFactory;
 use SimpleAsFuck\ApiToolkit\Model\Server\RequestRules;
+use SimpleAsFuck\Validator\Model\RuleChain;
+use SimpleAsFuck\Validator\Model\Validated;
+use SimpleAsFuck\Validator\Rule\String\StringRule;
 use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -17,5 +20,16 @@ final class Validator
         $factory = new PsrHttpFactory($psrFactory, $psrFactory, $psrFactory, $psrFactory);
         $request = $factory->createRequest($request);
         return new RequestRules(new BadRequestException(), $request);
+    }
+
+    /**
+     * @param non-empty-string $name
+     * @return StringRule
+     */
+    public static function parameter(?string $value, string $name): StringRule
+    {
+        /** @var Validated<mixed> $value */
+        $value = new Validated($value);
+        return new StringRule(new BadRequestException(), new RuleChain(), $value, 'Url path parameter: '.$name);
     }
 }

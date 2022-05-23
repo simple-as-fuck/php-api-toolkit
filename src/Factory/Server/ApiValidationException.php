1d8f733d1d7bf64606a49e1a34d2cad5ac436cad
VojtaB <triplkrypl@centrum.cz>
2022-05-23 23:11:16 +0200

    magic constant in ApiValidationException factory removed

--- src/Factory/Server/ApiValidationException.php
+++ src/Factory/Server/ApiValidationException.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace SimpleAsFuck\ApiToolkit\Factory\Server;
 
+use Kayex\HttpCodes;
 use SimpleAsFuck\Validator\Factory\Exception;
 
 final class ApiValidationException extends Exception
 {
     public function create(string $message): \Exception
     {
-        return new \SimpleAsFuck\ApiToolkit\Model\Server\ApiException($message, 400);
+        return new \SimpleAsFuck\ApiToolkit\Model\Server\ApiException($message, HttpCodes::HTTP_BAD_REQUEST);
     }
 }

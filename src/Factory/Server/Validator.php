b61cc1533cc0c7620e62314498da6bbada16d1d4
VojtaB <brozek@thepay.eu>
2023-11-30 17:07:29 +0100

    overriding of request validator exception type

--- src/Factory/Server/Validator.php
+++ src/Factory/Server/Validator.php
@@ -6,24 +6,25 @@ namespace SimpleAsFuck\ApiToolkit\Factory\Server;
 
 use Psr\Http\Message\ServerRequestInterface;
 use SimpleAsFuck\ApiToolkit\Model\Server\RequestRules;
+use SimpleAsFuck\Validator\Factory\Exception;
 use SimpleAsFuck\Validator\Model\RuleChain;
 use SimpleAsFuck\Validator\Model\Validated;
 use SimpleAsFuck\Validator\Rule\String\StringRule;
 
 final class Validator
 {
-    public static function make(ServerRequestInterface $request): RequestRules
+    public static function make(ServerRequestInterface $request, Exception $exception = new ApiValidationException()): RequestRules
     {
-        return new RequestRules(new ApiValidationException(), $request);
+        return new RequestRules($exception, $request);
     }
 
     /**
      * @param non-empty-string $name
      */
-    public static function parameter(?string $value, string $name): StringRule
+    public static function parameter(?string $value, string $name, Exception $exception = new ApiValidationException()): StringRule
     {
         /** @var Validated<mixed> $value */
         $value = new Validated($value);
-        return new StringRule(new ApiValidationException(), new RuleChain(), $value, 'Url path parameter: '.$name);
+        return new StringRule($exception, new RuleChain(), $value, 'Url path parameter: '.$name);
     }
 }
--- src/Factory/Symfony/Validator.php
+++ src/Factory/Symfony/Validator.php
@@ -6,6 +6,7 @@ namespace SimpleAsFuck\ApiToolkit\Factory\Symfony;
 
 use GuzzleHttp\Psr7\HttpFactory;
 use SimpleAsFuck\ApiToolkit\Model\Server\RequestRules;
+use SimpleAsFuck\Validator\Factory\Exception;
 use SimpleAsFuck\Validator\Model\RuleChain;
 use SimpleAsFuck\Validator\Model\Validated;
 use SimpleAsFuck\Validator\Rule\String\StringRule;
@@ -14,22 +15,21 @@ use Symfony\Component\HttpFoundation\Request;
 
 final class Validator
 {
-    public static function make(Request $request): RequestRules
+    public static function make(Request $request, Exception $exception = new BadRequestException()): RequestRules
     {
         $psrFactory = new HttpFactory();
         $factory = new PsrHttpFactory($psrFactory, $psrFactory, $psrFactory, $psrFactory);
         $request = $factory->createRequest($request);
-        return new RequestRules(new BadRequestException(), $request);
+        return new RequestRules($exception, $request);
     }
 
     /**
      * @param non-empty-string $name
-     * @return StringRule
      */
-    public static function parameter(?string $value, string $name): StringRule
+    public static function parameter(?string $value, string $name, Exception $exception = new BadRequestException()): StringRule
     {
         /** @var Validated<mixed> $value */
         $value = new Validated($value);
-        return new StringRule(new BadRequestException(), new RuleChain(), $value, 'Url path parameter: '.$name);
+        return new StringRule($exception, new RuleChain(), $value, 'Url path parameter: '.$name);
     }
 }

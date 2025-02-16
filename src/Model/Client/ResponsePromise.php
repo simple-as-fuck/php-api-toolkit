1c52616918990beafc320556786ae181fdb49bb8
VojtaB <triplkrypl@centrum.cz>
2025-02-16 19:32:22 +0100

    removed deprecated code

--- src/Model/Client/ResponsePromise.php
+++ src/Model/Client/ResponsePromise.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace SimpleAsFuck\ApiToolkit\Model\Client;
 
 use GuzzleHttp\Promise\PromiseInterface;
-use Psr\Http\Message\ResponseInterface;
 
 final class ResponsePromise
 {
@@ -17,32 +16,5 @@ final class ResponsePromise
         public readonly Request $request,
         public readonly PromiseInterface $promise,
     ) {
-    }
-
-    /**
-     * @deprecated will be removed
-     * @return non-empty-string
-     */
-    public function apiName(): string
-    {
-        return $this->apiName;
-    }
-
-    /**
-     * @deprecated will be removed
-     */
-    public function request(): Request
-    {
-        return $this->request;
-    }
-
-    /**
-     * @deprecated will be removed
-     */
-    public function wait(): Response
-    {
-        /** @var ResponseInterface $response */
-        $response = $this->promise->wait();
-        return new Response($this->request, $response);
     }
 }
